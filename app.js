b8419cc2f586f94dedd839f4f11605e85ebf824e
Muhammad-Anas16 <anas.hanf38@gmail.com>
2024-05-29 02:17:05 -0700

    console

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
 
-alert("please! Enter a valid password");
+// alert("please! Enter a valid password");
 
-alert("Welcome to Js land....\n   Happy coding");
+// alert("Welcome to Js land....\n   Happy coding");
 
-var fulName = ("Happy Coding !\nPrevert this page by adding a dialog");
+// var fulName = ("Happy Coding !\nPrevert this page by adding a dialog");
 
-alert(fulName);
+// alert(fulName);
 
+// var num = 10;
+
+// alert(num * 5);
+
+var inc = 30;
+
+console.log(inc)
+
+console.log(inc + 6)
